4e8e7e737d0033db23a76d1b8191bf68f0e641bf
fiveinfinity <jlachance1@gmail.com>
2016-03-02 09:06:57 -0600

    rspec passing

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+require 'date'
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    current_time = Time.now.hour
+
+    if current_time < 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+    resp.finish
+  end
+
+end
