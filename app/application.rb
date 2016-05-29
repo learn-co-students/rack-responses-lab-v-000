a4bb07a1e55b425089fe45e0b6375a1aa6df263c
kade042 <dereje88@gmail.com>
2016-05-29 11:00:27 -0600

    Done.

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+#require 'pry'
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    time = Time.now
+    if time.hour <= 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+    #binding.pry
+    resp.finish
+  end
+end
