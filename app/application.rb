7823a871a7f51dde978e1bf9612d52602cbba213
Christian Whitesides <whitesidescr@gmail.com>
2016-06-13 13:47:08 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,8 +4,7 @@ class Application
     resp = Rack::Response.new
 
     t = Time.now
-    
-    if t.hour > 12
+    if t.hour >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
