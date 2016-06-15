54e3b02f7aab8080c56b0e93e76396b381e718aa
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-06-15 14:04:39 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,14 @@
 class Application
   def call(env)
     resp = Rack::Response.new
+    now = Time.now
 
-    
-
+    if now.hour < 12
+      resp.write "Good Morning!"
+    elsif now.hour > 12
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
 
 
   end
