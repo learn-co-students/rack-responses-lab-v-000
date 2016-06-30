d54a4da878f1acb24c0d7dd24e2a2bfe7f686850
andrewstone8 <andrewstone8@gmail.com>
2016-06-30 23:18:31 +0900

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    if Time.now.hour > 12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
+    end
+
+
+    resp.finish
+  end
+
+end# => end class
