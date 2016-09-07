3d84e4679f1b218f6f3cc20652e9a13e9a6c260e
Kathleen DaSilva <kayjsmyth@gmail.com>
2016-09-07 15:59:51 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    time = Time.now
+    time_array = time.to_a
+    hour = time_array[2]
+    if hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
+  end
+end
