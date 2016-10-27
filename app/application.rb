df76764cf8801fad559b356b3640aac22905c4ea
Anna Lansfjord <lansfjord@gmail.com>
2016-10-27 20:50:45 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+      resp = Rack::Response.new
+      time1 = Time.now.hour
+      resp.write "#{time1}\n"
+
+      if time1 < 12
+        resp.write "Good Morning!"
+      else
+        resp.write "Good Afternoon!"
+      end
+    resp.finish
+  end
+end
