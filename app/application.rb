e45102732bee0a622790b237fb0038becd11fc3b
bharti gurnaney <bharti0717@yahoo.com>
2016-11-17 23:40:44 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+require "pry"
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    now= Time.now
+    hour = now.hour
+    
+    if hour < 12
+    resp.write "Good Morning"
+    else
+    resp.write "Good Afternoon"
+  end
+    resp.finish
+  end
+end
