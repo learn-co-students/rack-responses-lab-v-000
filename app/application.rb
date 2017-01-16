9bda88c4ea49d0d63636a29072a1f60add4e116e
brownielox <mrsmpython@gmail.com>
2017-01-16 12:14:12 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,9 @@
 class Application
 
-  def clock(env)
+  def call(env)
     resp = Rack::Response.new
     time = Time.now.hour
-    if time >= 0 && time <= 11
+    if time <= 11
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
