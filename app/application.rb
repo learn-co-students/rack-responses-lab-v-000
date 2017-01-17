c29aca85d4214f949c0003da79e99e98076f2978
Nicholas Ratti <nickratti@gmail.com>
2017-01-17 03:42:31 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+require 'pry'
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    t = Time.now
+    if t.hour > 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+    resp.finish
+  end
+end
