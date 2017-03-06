1369b0d2a53eb950d94f5ad2cbe2c7042d1a0b3c
Levio Marotti III <lm3@protonmail.com>
2017-03-06 02:22:27 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     now = Time.now
-    if now.hour >=12
+    if now.hour >= 12
       resp.write "Good Afternoon"
     else
       resp.write "Good Morning"
