2d063bc3d14639c855c5aa1b7c06f9def156f1a6
WilliamBarela <william.barela.dev@gmail.com>
2017-05-16 00:08:59 +0900

    Done.

---
+++ app/application.rb
@@ -0,0 +1,10 @@
+require "time"
+
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    hour = Time.now.hour
+    (hour >= 0 && hour < 12) ? (resp.write "Good Morning") :  (resp.write "Good Afternoon")
+    resp.finish
+  end
+end
