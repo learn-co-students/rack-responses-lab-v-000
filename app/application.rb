733178c6e09fca432d72ec8b10d44c66247ab87c
github username <github email address>
2017-08-26 14:47:22 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    t = Time.now.localtime.hour
+
+    if t < 12
+      resp.write "Good Morning!\n"
+    else
+      resp.write "Good Afternoon!\n"
+    end
+
+    resp.write "The current time is #{Time.now.localtime.asctime}"
+    resp.finish
+  end
+
+end
