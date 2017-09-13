b8f3c399813e7e1d59d807582551b45dc6fab7a3
NickisKnowledge <lifechangingstuff@gmail.com>
2017-09-13 17:41:20 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
 
     time = Time.now.strftime("%H").to_i
-    resp.write "Good #{((00...12).include? time) ? 'Morning' : 'Afternoon' }"
+    resp.write "Good #{ ((00...12).include? time) ? 'Morning' : 'Afternoon' }"
 
     resp.finish
   end
