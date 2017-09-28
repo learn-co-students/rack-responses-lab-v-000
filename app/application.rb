e815fb8f54c9202d615a9b931f5c913b18591b54
Christian Egglin <cegglin@conncoll.edu>
2017-09-28 21:53:04 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
     resp = Rack::Response.new
 
     if Time.now.hour < 12
-      resp.write "Good Morning"
+      resp.write "Good Morning!"
     else
-      resp.write "Good Afternoon"
+      resp.write "Good Afternoon!"
     end
     resp.finish
   end
