fd17846ebb8f36ecf0e53fbbff6973c001ca22f4
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-10-01 10:25:16 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    current_time = Time.new()
+    current_time = Time.now
     if current_time.hour >= 12
       resp.write "Good Afternoon"
     else
