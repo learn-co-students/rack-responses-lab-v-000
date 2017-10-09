f2acda37be01a8f746ebe2949df64604bdd88535
Alexander Friedland <friedlandaj@gmail.com>
2017-10-09 03:55:14 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+
+    time = Time.now
+    resp.write "Current date and time is: #{time}"
+
+    if time.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
+  end
+end
