64e527ead6d39bac957e01082a4433d60b20c2f7
Amanda <amandarebecca89@gmail.com>
2017-10-15 12:00:07 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
        t = Time.now.hour
       if t < 12
         resp.write "Good Morning!"
-      elsif t >= 12
+      else t >= 12
         resp.write "Good Afternoon!"
       end
 
