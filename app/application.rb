0c58202a7a697f3455e0b470e22388819af3c865
Emma Feng <ff18qd@gmail.com>
2017-11-14 01:19:58 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    t = Time.now
+    hour = t.hour
+    if hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
+  end
+
+end
