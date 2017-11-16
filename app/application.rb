c4bfdd597bd7048cc942bd2d61f6188e5965794a
Yehuda Makarov <yehudamakarov@gmail.com>
2017-11-15 19:20:44 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+  def call(env)
+
+    resp = Rack::Response.new
+    time = Time.new.hour
+    if time < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+    resp.finish
+  end
+end
