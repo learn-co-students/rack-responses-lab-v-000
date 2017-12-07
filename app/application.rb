1ecc83e3f46f19105d41330ee5efbd671735fe45
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-07 00:26:15 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    hour = Time.now.hour
+    if hour > 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+    resp.finish
+  end
+
+end
