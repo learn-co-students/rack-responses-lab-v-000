e7b485c31ef0255c48af14459f37bf7c5593fab7
Lara Sanders <larasanders@gmail.com>
2017-12-10 19:26:32 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+  time = Time.now
 
-    if datetime <= 12:00 && > 23:59
+
+    #  morning = Kernel <= "12:00\n" && Kernel > "23:59\n"
+    #   num_2 = Kernel.rand(1..20)
+    if time.hour <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
