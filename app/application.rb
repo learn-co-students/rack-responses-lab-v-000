7f2e2239846da6126a40c4383fa54076bb4c5d90
Sally Zasloff <szasloff@gmail.com>
2018-01-23 22:49:39 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+  def call (env)
+    resp = Rack::Response.new
+
+    t = Time.now
+    resp.write "#{t.strftime("%H:%M")}\n"
+
+
+    if t.hour <= 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+
+    resp.finish
+
+  end
+end
