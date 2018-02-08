61781ba497e3752ca379a4ae3aaab962ed50fa3d
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-08 18:35:38 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    t = Time.now
+    msg = (t.hour > 12) ? "Good Afternoon!" : "Good Morning!"
+    resp.write msg
+
+    resp.finish
+  end
+end
