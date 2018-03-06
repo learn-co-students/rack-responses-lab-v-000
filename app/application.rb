8234eb36a79dd1a5fff381a7a3bd9b5628c537d5
ElleeB <laura.trager@gmail.com>
2018-03-06 14:16:35 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     t = Time.now
     hour = t.hour
 
-    if hour.between?(1, 11)
+    if hour <= 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
