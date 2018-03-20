b3a96a6e27ad8ed381eefb824437fcd61888c5a2
Eden Mesfun <emesfun@gmail.com>
2018-03-20 14:07:14 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    time = Time.now
+
+    if time.hour < 12
+      resp.write "Morning"
+    else
+      resp.write "Afternoon"
+    end
+      resp.finish
+  end
+
+end
