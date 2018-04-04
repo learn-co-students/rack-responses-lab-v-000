209749bdf4a989e5ba0c2797bbcc9c9f18821639
Kelsey Fecho <kfecho30@gmail.com>
2018-04-04 00:22:17 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    time = Time.now.getlocal('-05:00').to_a[2]
+    time = Time.now.to_a[2]
     if time > 12
       resp.write "Good Afternoon!\n"
     else
