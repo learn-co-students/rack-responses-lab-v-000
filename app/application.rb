1294d174a31cb151aa4d54ca8692118accb3080b
Sara Stanton <sara.mpa13@gmail.com>
2018-04-24 04:27:38 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    resp.write "#{Time.now}"
+
+    if Time.now.hour > 12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
+    end
+
+    resp.finish
+  end
+
+end
