172a2679c0d1313ddaf0bcef1936d44dc27cb1c3
Elle Wilson <eleanorwilsonn@gmail.com>
2018-04-30 17:22:36 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    now = Time.now
+   if now.hour >=12
+      resp.write "Afternoon"
+    else
+      resp.write "Morning"
+    end
+
+    resp.finish
+  end
+
+end
