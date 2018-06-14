90a605e502937880d878a988ffc386a7c98ad6f6
pcherian10 <pcherian10@gmail.com>
2018-06-13 21:49:29 -0400

    complete

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+
+class  Application
+
+	def call(env)
+		resp = Rack::Response.new
+
+    current_time = []
+    current_time << Time.now.to_s
+    current_time = current_time[0].split(" ")[1]
+    current_time = current_time.split(":")[0].to_i
+
+  	if(current_time < 12)
+  	   resp.write "Good Morning!"
+  	else
+  		resp.write "Good Afternoon!"
+  	end
+
+		resp.finish
+  end
+
+
+end
