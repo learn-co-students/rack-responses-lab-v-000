c45865361989ab9fb34a9f552a62de1a075f0910
Ron Sala <ron.sala@gmail.com>
2018-06-20 05:58:22 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.now
+    time = Time.now.hour
     # binding.pry
     if time < 12
       resp.write "Good Morning!"
