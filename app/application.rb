6f0cb4a2b7658025f30efd178be6970b1a225a80
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-04 17:44:05 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    current_time = Time.new
-    meridian = current_time.strftime('%p')
+    current_time = Time.now
     
-    if meridian.downcase == 'pm'
+    if current_time.hour < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
