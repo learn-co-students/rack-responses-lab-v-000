738d6b592333bf71fcfc8015ca0b37444881d943
veloblank <veloblank@gmail.com>
2018-07-21 23:00:00 -0400

    Added 'Good Evening' greeting functionality.

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    current_time = Time.now
+    current_time = "Time.now"
 
-    if current_time.strftime("%H:%M:%S") > "12:00:00"
-      resp.write "Good Afternoon"
+    if current_time.strftime("%H:%M:%S") >= "17:00:00"
+      resp.write "Good Evening"
+    elsif current_time.strftime("%H:%M:%S") <= "15:00:00" && current_time.strftime("%H:%M:%S") >= "12:00:00"
+        resp.write "Good Afternoon"
     else
       resp.write "Good Morning"
     end
