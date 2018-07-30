095be83027934f099fd95ef511de3d40a3c7dc5e
Timothy Ege <retibra@gmail.com>
2018-07-30 12:35:13 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,20 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    time = Time.now
+
+    resp.write "#{time}\n"
+
+
+    if time.hour >= 12
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
+
+    resp.finish
+  end
+
+end
