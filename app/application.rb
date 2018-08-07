82834c236f45e4a55ecbe094c63b3beff13efe07
Brittany Tinnin <bntinnin7@gmail.com>
2018-08-07 11:49:25 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+
+    if Time.now.hour < 12
+      resp.write "Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+
+    resp.finish
+  end
+end
