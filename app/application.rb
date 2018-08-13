3ac65f383c6d1d71818311e99b9eb179f7d6d46a
Nikki A <nikki.apostolakis@gmail.com>
2018-08-13 15:40:00 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     #resp.write "#{Time.now} \n"
     #resp.write "#{Time.now.hour}"
 
-    if Time.now.hour > 12
+    if Time.now.hour >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning"
