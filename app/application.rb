ed24cc1864cd6d9ac5d213ca94730d166457d121
Dani Schuhman <dani8439@aol.com>
2018-08-15 16:53:10 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,19 @@
 class Application
-
   def call(env)
     resp = Rack::Response.new
-    now = Time.now.to_i
-    # now = Time.now
 
-    if now < 1448625600
-    # if now.hour >= 12 --> converts to hour
-    # resp.write "Good Afternoon!"
+    t = Time.now
+    # returns => 2018-8-15 12:52:20 -600 
+    # t.hour parses out the hour -- no need to convert .to_i
+
+    if t.hour < 12 
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
-
     end
+
     resp.finish
   end
 end
+
+
