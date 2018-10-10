5a971e37647e023a2c7103e081fd892945042961
Justine Bricknell <justinegayton@gmail.com>
2018-10-10 23:15:09 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    if Time.now.hour.between?(0,11)
+      resp.write "Good Morning!"
+    else Time.now.hour.between?(12,17)
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
