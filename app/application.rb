a52843844bc093f3ddc5157692bfe5f5d1061a36
Stef Claus <stef.claus@gmail.com>
2019-01-27 19:05:42 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    current_time = Time.now
+    
+
+    if
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
