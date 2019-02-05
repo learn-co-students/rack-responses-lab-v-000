1331f1e8e58542893e911a467d35be140136e7f7
Ashley Wong <ashwong04@gmail.com>
2019-02-05 20:48:02 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,11 +4,7 @@ class Application
     resp = Rack::Response.new
 
 
-  if num_1 == num_2 && num_2 == num_3
-    resp.write "You Win"
-  else
-    resp.write "You Lose"
-  end
+  
     resp.finish
   end
 end
