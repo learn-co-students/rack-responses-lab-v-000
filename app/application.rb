fe2cd9a815212e700ef2c7d1aa425580ae4b2c78
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-18 04:27:24 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+  #
+  def call(env)
+    resp = Rack::Response.new
+    resp.write "Good Morning!"
+    resp.finish
+  #
+    time = Time.now
+
+    if time.hour < 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+    resp.finish
+  end
+end
