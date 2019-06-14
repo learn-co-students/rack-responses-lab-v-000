e751f5f78c91801a957273b84c6790576c4d3bfa
Steven Crouse <sdcrouse91@gmail.com>
2019-06-14 00:39:00 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -4,13 +4,15 @@ class Application
   
   def call(env)
     # I now know that the time that the tests pass to this method is Time.now
-    # if Time.now < noon, resp.write "Good Morning", else "Good AFternoon!"
+    # if Time.now < noon, resp.write "Good Morning", else "Good Afternoon!"
     # I could also write: if Time.now.strftime("%H:%M:%S") < "12:00:00"
     # See https://ruby-doc.org/core-2.2.0/Time.html#method-i-strftime
     
     resp = Rack::Response.new
     
-    if Time.now.hour < 12 # Use <, not <=, or "12:30:00".hour will output "Good Morning!"
+    #resp.write "#{Time.new.utc?}"
+    
+    if Time.now.localtime.hour < 12 # Use <, not <=, or "12:30:00".hour will output "Good Morning!"
       resp.write "Good Morning!"
     else 
       resp.write "Good Afternoon!"
