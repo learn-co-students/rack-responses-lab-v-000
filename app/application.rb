332f9af3af337cdc58ffffc3e7e1db96c6a83751
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-06-15 00:18:38 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,11 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new
+      
+      resp.write "Good Morning!"
+      resp.write "Good Afternoon!"
+    
+    resp.finish
+  end
+end
