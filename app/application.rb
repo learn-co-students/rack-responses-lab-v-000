f262af475c61a82c607cfdd8f86c224750cabf72
Olivia Kesler <oakesler@protonmail.ch>
2019-12-14 22:09:49 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    resp.write "Hello, my name is "
+    resp.finish
+  end
+
+end
+
+
+require 'rack'
+ 
+# Something that responds to call, that's what Rack demands
+class MyServer
+  def call(env)
+    return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
+  end
+ 
+  def pretty_response
+    (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
+  end
+end
+ 
+run MyServer.new
