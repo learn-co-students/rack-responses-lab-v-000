0ac7634d1ac104bfcc7c01b3263550772159b83b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-03-11 23:26:55 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+  def call(env)
+
+    resp = Rack::Response.new
+
+    if (0..11).include?(Time.now.hour)
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
