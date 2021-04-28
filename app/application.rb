ce355065e2c0478e69626472b568aa39e49f4138
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-27 23:05:22 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -3,13 +3,13 @@ class Application
     resp = Rack::Response.new
     time = Time.now.to_i
 
-    if time < 12:00:00
+    if time < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
     end
-
     resp.finish
+  end
 end
 
 # If it's before noon, greet the user with "Good Morning!"
