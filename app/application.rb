998769e4eeee5f9b5ae65b0a66eb2db49531527e
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-27 23:03:22 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -16,3 +16,22 @@ end
 # If it's after noon, greet the user with "Good Afternoon!". 
 # Remember that getting the time may return results in 24 hour time! 
 # Anytime on or after 12 is the afternoon.
+
+#   def call(env)
+#     resp = Rack::Response.new
+#     num_1 = Kernel.rand(1..20)
+#     num_2 = Kernel.rand(1..20)
+#     num_3 = Kernel.rand(1..20)
+
+#     resp.write "#{num_1}\n"
+#     resp.write "#{num_2}\n"
+#     resp.write "#{num_3}\n"
+
+#     if num_1 == num_2 && num_2 == num_3
+#       resp.write "You Win"
+#     else
+#       resp.write "You Lose" 
+#     end
+
+#     resp.finish
+#   end
