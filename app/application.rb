ca420dcfa1701342de6f82f18f33176144b26584
Daniel Suarez <suarez.daniel101@gmail.com>
2021-04-27 23:33:50 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,9 @@
 class Application
   def call(env)
     resp = Rack::Response.new
-    time = Time.new * rand
+    time = Time.new
 
-    if time.strftime("%k:%M").to_i < 12
+    if rand(time.hour) < 12
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
@@ -17,21 +17,3 @@ end
 # Remember that getting the time may return results in 24 hour time! 
 # Anytime on or after 12 is the afternoon.
 
-#   def call(env)
-#     resp = Rack::Response.new
-#     num_1 = Kernel.rand(1..20)
-#     num_2 = Kernel.rand(1..20)
-#     num_3 = Kernel.rand(1..20)
-
-#     resp.write "#{num_1}\n"
-#     resp.write "#{num_2}\n"
-#     resp.write "#{num_3}\n"
-
-#     if num_1 == num_2 && num_2 == num_3
-#       resp.write "You Win"
-#     else
-#       resp.write "You Lose" 
-#     end
-
-#     resp.finish
-#   end
